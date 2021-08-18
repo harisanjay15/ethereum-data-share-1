3dd53394cbec889c789c486ffbb3279857c3d8c7
harisanjay15 <44949415+harisanjay15@users.noreply.github.com>
2021-08-18 22:38:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,13 @@ import React, {Component} from 'react';
 import Web3 from 'web3';
 import './App.css';
 import File from '../abis/File.json';
-
 import {ToastsContainer, ToastsStore} from 'react-toasts';
+//let CryptoJS = require("crypto-js");
 
+
+
+let count=0;
+let encrypted;
 class App extends Component {
     constructor(props) {
         super(props);
@@ -13,11 +17,13 @@ class App extends Component {
             authenticated: '',
             contract: null,
             status: "Not Uploaded",
-            unitCode: '',
-            unitMarks: '',
-            studentID: '',
+            patientName: '',
+            patientStatus: '',
+            patientID: '',
             data: '',
-            password: ''
+            students: [],
+            password: '',
+            count: 0
         }
     }
 
@@ -56,20 +62,20 @@ class App extends Component {
         }
     }
 
-    updateUnit = (e) => {
+    updateName = (e) => {
         e.preventDefault();
-        this.setState({unitCode: e.target.value});
+        this.setState({patientName: e.target.value});
     };
 
 
-    updateMarks = (e) => {
+    updateStatus = (e) => {
         e.preventDefault();
-        this.setState({unitMarks: e.target.value});
+        this.setState({patientStatus: e.target.value});
     };
 
     updateID = (e) => {
         e.preventDefault();
-        this.setState({studentID: e.target.value});
+        this.setState({patientID: e.target.value});
     };
 
     updatePassword = (e) => {
@@ -79,13 +85,15 @@ class App extends Component {
 
     onSubmit = (e) => {
         e.preventDefault();
-        let code = this.state.unitCode;
-        let marks = this.state.unitMarks;
-        let ID = this.state.studentID;
-        this.state.data = this.state.data + ID + "-" + code + "-" + marks + ":";
+        let name = this.state.patientName;
+        let ID = this.state.patientID;
+        let status = this.state.patientStatus;
+        this.state.data = this.state.data + name + "-" + ID + "-" + status + ":";
+        //encrypted =CryptoJS.AES.encrypt(this.state.data, '123wredfsdf').toString();
+        //console.log(encrypted);
         ToastsStore.warning("Adding data to Blockchain...");
 
-
+        count= count+1;
         // Store data on Blockchain
         this.state.contract.methods.set(this.state.data).send({from: this.state.account}).on("confirmation", (r) => {
             console.log("Data stored on Blockchain...");
@@ -114,6 +122,7 @@ class App extends Component {
             console.log(this.state.data);
             ToastsStore.success("Data fetched successfully!");
         });
+        this.updateData();
     };
 
     authenticate = (e) => {
@@ -130,11 +139,60 @@ class App extends Component {
         }
     };
 
+    updateData = () => {
+        //this.state.data= this.state.data+" ";
+        let data;
+        let i;
+        let patientID_ = "Not Found";
+        let patientCode_ = "Not Found";
+        let patientStatus_ = "Not Found";
+        //var bytes  = CryptoJS.AES.decrypt(encrypted, '123wredfsdf');
+        //var originalText = bytes.toString(CryptoJS.enc.Utf8);
+
+        try{
+        data = this.state.data.split(":");
+       
+        this.state.students = this.state.data.split(":");
+        
+             let patientData = data[0].split("-");
+                 patientID_ = patientData[0];
+                  patientCode_ = patientData[1];
+                  patientStatus_ = patientData[2];
+                  //originalText=data[0];
+    }
+    catch(err)
+    {
+        console.log(data);
+    }
+        this.setState({patientID_, patientCode_, patientStatus_});
+    };
+
+    openPage(pageName,elmnt,color) {
+        console.log("funvctionnnn.....");
+        pageName="add";
+        let i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablink");
+        for (i = 0; i < tablinks.length; i++) {
+           tablinks[i].style.backgroundColor = "";
+        }
+        if(pageName == null)
+        {
+        document.getElementById(pageName).style.display = "block";
+        elmnt.style.backgroundColor = color;
+        
+        
+        document.getElementById("defaultOpen").click();}
+    }
+
     render() {
         return (
             <div>
                 <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-                    <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="http://www.jaykch.com/"
+                    <a className="navbar-brand col-sm-3 col-md-2 mr-0" 
                        target="_blank" rel="noopener noreferrer">
                         Admin Panel
                     </a>
@@ -152,50 +210,130 @@ class App extends Component {
                 <div className="container-fluid mt-5">
                     <div className="row">
                         <ToastsContainer store={ToastsStore}/>
-                        <main role="main" className="col-lg-12 d-flex text-center">
+                        <main role="main"  className="col-lg-12 d-flex text-center">
                             <div className="content mr-auto ml-auto">
                                 <br/>
+
                                 {this.state.authenticated ?
-                                    <form onSubmit={this.onSubmit}>
-                                        <div className="form-group row">
-                                            <label htmlFor="studentID"
-                                                   className="col-sm-12 col-form-label">Student ID:</label>
-                                            <div className="col-sm-12">
-                                                <input type="text" className="form-control" id="studentID"
-                                                       value={this.state.studentID} placeholder="Enter Student ID!"
+
+                                    <div>
+                            
+                                    <div id="add">
+                                        <form onSubmit={this.onSubmit}>
+                                            <div className="form-group row">
+                                                <label htmlFor="patientName"
+                                                       className="col-sm-12 col-form-label">Patient Name:</label>
+                                                <div className="col-sm-12">
+                                                    <input type="text" className="form-control" id="patientName"
+                                                       value={this.state.patientName} placeholder="Enter Patient Name!" required
+                                                       onChange={this.updateName}/>
+                                                </div>
+                                            </div>
+
+                                            <div className="form-group row">
+                                                <label htmlFor="PatientID"
+                                                       className="col-sm-12 col-form-label">Patient ID:</label>
+                                                <div className="col-sm-12">
+                                                    <input type="text" className="form-control" id="patientID"
+                                                       value={this.state.patientID} placeholder="Enter Patient ID!" required
                                                        onChange={this.updateID}/>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className="form-group row">
-                                            <label htmlFor="unitCode"
-                                                   className="col-sm-12 col-form-label">Unit:</label>
-                                            <div className="col-sm-12">
-                                                <input type="text" className="form-control" id="unitCode"
-                                                       value={this.state.unitCode} placeholder="Enter Unit Code!"
-                                                       onChange={this.updateUnit}/>
+
+                                            <div className="form-group row">
+                                                <label htmlFor="testResult"
+                                                   className="col-sm-12 col-form-label">Covid Test Result:</label>
+                                                <div className="col-sm-12">
+                                                    <input type="text" className="form-control" id="testResult"
+                                                       value={this.state.patientStatus} placeholder="Enter Test Results!" required
+                                                       onChange={this.updateStatus}/>
+                                                </div>
                                             </div>
-                                        </div>
+
+                                            <button type="button" onClick={this.onSubmit}
+                                                className="premium-button">Submit
+                                            </button>
+
+                                            <button type="button" onClick={this.reset}
+                                                className="premium-button">Reset Data
+                                            </button>
+
+
+                                        </form>
+                                    </div>
+
+                                    <div id="check">
+                                        <form>
                                         <div className="form-group row">
                                             <label htmlFor="unitMarks"
-                                                   className="col-sm-12 col-form-label">Marks:</label>
+                                                   className="col-sm-12 col-form-label">Search by Test ID:</label>
                                             <div className="col-sm-12">
-                                                <input type="text" className="form-control" id="unitMarks"
-                                                       value={this.state.unitMarks} placeholder="Enter Marks!"
-                                                       onChange={this.updateMarks}/>
+                                                <input type="text" className="form-control" id="PID"
+                                                        placeholder="Enter Test ID" required
+                                                       />
                                             </div>
                                         </div>
-                                        <button type="button" onClick={this.onSubmit}
-                                                className="premium-button">Submit
+                                        <button type="button" onClick={this.fetchData} className="premium-button">
+                                                Display
                                         </button>
-                                        <button type="button" onClick={this.reset}
-                                                className="premium-button">Reset Data
-                                        </button>
-                                    </form>
+                                        <u1>
+                                        {       
+                                        
+                                                this.state.students.map(function(item, i){
+                                                    
+                                                    if(item.length>1){
+                                                        let patientData = item.split("-");
+                                                        let patientName = patientData[0];
+                                                        let patientID = patientData[1];
+                                                        let patientStatus = patientData[2];
+                                                        let id = document.getElementById("PID").value
+                                                        if(patientID === id)
+                                                        {
+                                                        return <li key={i}>
+                                                            <h2>Patient Name: {patientName}</h2>
+                                                            <h2>Test ID: {patientID}</h2>
+                                                            <h2>Covid Test Result: {patientStatus}</h2>
+                                                        </li>
+                                                        }
+                                                        else
+                                                        {
+                                                            
+                                                            if((i+1)>=count)
+                                                            {
+                                                                
+                                                                return <h2> Not Found</h2>
+                                                                        
+                                                            }
+                                                        }
+                                                       
+                                                    }
+                                                    
+                                                })
+
+                                                
+                                            }
+                                            </u1>
+
+                                        </form>
+
+                                    </div>
+
+                                    
+
+                                    </div>
+                                    
                                     :
                                     <div>
                                         <h2>Not Authenticated! </h2>
                                         <form onSubmit={this.authenticate}>
                                             <div className="form-group row">
+                                            <label htmlFor="username"
+                                                       className="col-sm-12 col-form-label">User Name:</label>
+                                                <div className="col-sm-12">
+                                                    <input type="text" className="form-control" id="username"
+                                                            placeholder="Enter Username"
+                                                           />
+                                                </div>
                                                 <label htmlFor="password"
                                                        className="col-sm-12 col-form-label">Password:</label>
                                                 <div className="col-sm-12">
@@ -208,7 +346,9 @@ class App extends Component {
                                                     className="premium-button">Submit
                                             </button>
                                         </form>
-                                    </div>}
+                                    </div>
+                                    
+                                    }
                             </div>
                         </main>
                     </div>
